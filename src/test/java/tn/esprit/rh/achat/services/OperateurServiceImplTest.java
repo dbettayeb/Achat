73ebfadf7f0a835615305d6bf1a8b2648a519d1c
MariemArif <78935782+MariemArif@users.noreply.github.com>
2022-10-27 20:37:39 +0100

    first commit

---
+++ src/test/java/tn/esprit/rh/achat/services/OperateurServiceImplTest.java
@@ -0,0 +1,51 @@
+package tn.esprit.rh.achat.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import tn.esprit.rh.achat.entities.Operateur;
+import tn.esprit.rh.achat.repositories.OperateurRepository;
+
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class OperateurServiceImplTest {
+
+	
+	@Mock
+	
+	OperateurRepository operateurRepository;
+	
+	@InjectMocks
+	OperateurServiceImpl OperateurService;
+
+	
+	Operateur op = new Operateur("op" , "lastop" , "f465465df");
+	
+	
+	
+	@Test
+	public List<Operateur> TestretrieveAllOperateurs() {
+		return (List<Operateur>) operateurRepository.findAll();
+	}
+
+	@Test
+	public void testRetrieveUser() {
+	Mockito.when(operateurRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(op));
+	Operateur op1 = OperateurService.retrieveOperateur(op.getIdOperateur());
+	Assertions.assertNotNull(op1);
+	}
+
+	
+
+}
